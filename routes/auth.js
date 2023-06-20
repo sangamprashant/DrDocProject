4b47657a3c1ee2c41ff048ab0b04ea0c4d267dff
Prashant Srivastav <srivastavp891@gmail.com>
2023-06-20 18:03:46 +0530

    auth signin

--- routes/auth.js
+++ routes/auth.js
@@ -63,11 +63,9 @@ router.post("/api/signup", (req, res) => {
               process.env.JWT_SECRET
             );
             const { _id, name, email, userName, account, Photo } = savedUser;
-            // Check if Photo is null and set it to an empty string
-        const userPhoto = Photo || "";
             res.json({
               token,
-              user: { _id, name, email, userName, account, Photo:userPhoto },
+              user: { _id, name, email, userName, account, Photo},
             });
           } else {
             return res.status(422).json({ error: "Invalid Pasword" });
